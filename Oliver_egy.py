533e401bd104b8e2b4823d2e438c304996d22cfe
mq44xe <mate.ruzsovits@stud.uni-corvinus.hu>
2023-04-25 10:43:06 +0200

    Peter

--- Oliver_egy.py
+++ Oliver_egy.py
@@ -7,62 +7,95 @@ screen = turtle.Screen()
 t = turtle.Turtle()
 t.speed(0)
 
-# Draw the head of the dog
-t.fillcolor("#8B4513")
+# Draw the head of Peter Griffin
+t.fillcolor("#F2C640")
 t.begin_fill()
-for i in range(36):
-    t.forward(10)
-    t.right(10)
-for i in range(18):
-    t.forward(5)
-    t.right(10)
-t.end_fill()
-
-# Draw the ears
-t.penup()
-t.goto(-50, 50)
-t.pendown()
-t.fillcolor("#8B4513")
-t.begin_fill()
-for i in range(4):
-    t.forward(40)
-    t.right(90)
-t.end_fill()
-
-t.penup()
-t.goto(50, 50)
-t.pendown()
-t.fillcolor("#8B4513")
-t.begin_fill()
-for i in range(4):
-    t.forward(40)
-    t.right(90)
+t.circle(60)
 t.end_fill()
 
 # Draw the eyes
 t.penup()
-t.goto(-20, 30)
+t.goto(-30, 70)
 t.pendown()
 t.fillcolor("#FFFFFF")
 t.begin_fill()
-t.circle(10)
+t.circle(20)
 t.end_fill()
 
 t.penup()
-t.goto(20, 30)
+t.goto(30, 70)
 t.pendown()
 t.fillcolor("#FFFFFF")
 t.begin_fill()
-t.circle(10)
+t.circle(20)
 t.end_fill()
 
-# Draw the nose
 t.penup()
-t.goto(0, 0)
+t.goto(-25, 75)
 t.pendown()
 t.fillcolor("#000000")
 t.begin_fill()
-t.circle(5)
+t.circle(10)
+t.end_fill()
+
+t.penup()
+t.goto(25, 75)
+t.pendown()
+t.fillcolor("#000000")
+t.begin_fill()
+t.circle(10)
+t.end_fill()
+
+# Draw the mouth
+t.penup()
+t.goto(-30, 50)
+t.pendown()
+t.right(45)
+t.forward(40)
+t.left(90)
+t.forward(40)
+t.left(90)
+t.forward(40)
+t.left(90)
+t.forward(40)
+t.left(135)
+
+# Draw the body
+t.penup()
+t.goto(0, -80)
+t.pendown()
+t.fillcolor("#F2C640")
+t.begin_fill()
+for i in range(2):
+    t.forward(80)
+    t.right(90)
+    t.forward(150)
+    t.right(90)
+t.end_fill()
+
+# Draw the legs
+t.penup()
+t.goto(-30, -80)
+t.pendown()
+t.fillcolor("#FFFFFF")
+t.begin_fill()
+for i in range(2):
+    t.forward(40)
+    t.right(90)
+    t.forward(80)
+    t.right(90)
+t.end_fill()
+
+t.penup()
+t.goto(30, -80)
+t.pendown()
+t.fillcolor("#FFFFFF")
+t.begin_fill()
+for i in range(2):
+    t.forward(40)
+    t.right(90)
+    t.forward(80)
+    t.right(90)
 t.end_fill()
 
 # Hide the turtle and exit on click
